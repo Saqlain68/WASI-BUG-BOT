87d5f627a071767334ec12514dad181f0081aecc
Saqlain68 <saqlainrajpoot197@gmail.com>
2024-09-22 22:38:15 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,14 +3,14 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "923192173398"
-global.ownername = "🐛 MR WASI "
+global.ownernomer = "923106081965"
+global.ownername = "🐛 the saqlain "
 global.ytname = "YT: Dont have"
 global.socialm = "GitHub: private"
-global.location = "Pakistan, Lahore"
+global.location = "Pakistan, Lokara"
 
-global.ownernumber = '923347748038'  //creator number
-global.ownername = ' MR WASI ' //owner name
+global.ownernumber = '923106081965'  //creator number
+global.ownername = ' the Saqlain' //ame
 global.botname = 'WASI TECH BUG BOT ' //name of the bot
 
 //sticker details
